19fc698dddf0eef7fc7a7498c1efc7319f50dba3
onlineresultsite <footflux@gmail.com>
2024-07-09 18:09:19 +0530

    ip change

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,13 +1,19 @@
-
 #!/usr/bin/bash
 
+# Reload the systemd manager configuration
 sudo systemctl daemon-reload
+
+# Remove the default Nginx site configuration
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/blogprojectdrf-master/nginx/nginx.conf /etc/nginx/sites-available/blog/settings.py
-sudo ln -s /etc/nginx/sites-available/blog/settings.py /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-#sudo nginx -t
-sudo gpasswd -a www-data ubuntu
-sudo systemctl restart nginx
+# Copy the Nginx configuration file to the sites-available directory
+sudo cp /home/ubuntu/blogprojectdrf-master/nginx/nginx.conf /etc/nginx/sites-available/blog
 
+# Create a symbolic link in the sites-enabled directory
+sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
+
+# Add the 'ubuntu' user to the 'www-data' group
+sudo gpasswd -a www-data ubuntu
+
+# Restart the Nginx service
+sudo systemctl restart nginx
