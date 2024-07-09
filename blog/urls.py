cc528a5636a1424b50ccedeced9aa481528170ea
onlineresultsite <footflux@gmail.com>
2024-07-09 17:27:04 +0530

    all

--- blog/urls.py
+++ blog/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view as swagger_get_schema_view
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
+from drf_yasg import openapi #new foe swagger
+from drf_yasg.views import get_schema_view as swagger_get_schema_view #new foe swagger
+
 
 schema_view = swagger_get_schema_view(
     openapi.Info(
@@ -33,8 +34,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
-]
 
+]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+    
